e2e2befef2ba8b87e5986d5848862d158ed48dec
Larissa Ferreira <o.larissax@gmail.com>
2023-03-31 14:15:47 -0300

    Input removido de import angular core em hero detail component

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,5 @@
 import { Location } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Hero } from '../hero.model';
 import { HeroService } from '../hero.service';
